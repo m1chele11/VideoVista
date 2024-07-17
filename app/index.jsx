c25dcbbfae4d27f8b3eb3ac2f8257783fc103e3d
michele <mbarrant@iu.edu>
2024-07-17 09:01:01 -0400

    pushing updates

--- app/index.jsx
+++ app/index.jsx
@@ -1,27 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import {Link} from 'expo-router';
+import {Text, View } from 'react-native';
+
 
 
 
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Ora</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{color: 'blue'}}>
-      Go to profile
-      </Link>
-    </View>
-  );
-}
+    return (
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className='text-3xl'>Hello</Text>
+        <StatusBar style="auto"/>
+        <Link href="/profile" style={{ color: 'blue'}}>
+        Go to profile
+        </Link>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    </View>
+    );
+}
